9f9aec9049ecf9ebd2420c6c981359a0bc1c3ea2
Milind Jamnekar <milind.jamnekar@neomindlab.de>
2023-07-01 22:01:39 +0530

    basic page added

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,16 @@
+import Signin from "@/components/Signin";
+import { buttonVariants } from "@/components/ui/Button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+export default function Page({}) {
+  return (
+    <section className="absolute inset-0">
+      <div className="h-full max-w-7xl mx-auto flex flex-col items-center justify-center">
+        <Link href="/" className={cn(buttonVariants({ variant: "ghost" }))}>
+          home
+        </Link>
+      </div>
+    </section>
+  );
+}
